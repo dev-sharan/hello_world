0d0affae018841ad83bcd6103c850e4e66277bbf
Biplob <biplobsd11@gmail.com>
2020-05-14 14:09:02 +0000

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home()
-));
+void main() => runApp(MaterialApp(home: Home()));
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text("App bar TEXTee"),
-      centerTitle: true,
-      backgroundColor: Colors.blue[600]
-    ),
-    body: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-    Container(
-      color: Colors.red,
-      padding: EdgeInsets.all(50),
-      margin: EdgeInsets.all(50),
-      child: Center(child: IconButton(
-          onPressed: () {
-            print("Clicked!!");
-          },
-          icon: Icon(
-            Icons.timer,
-            color: Colors.amber
-          ),
-        ),
-        ),
-      ), 
-      FlatButton(onPressed: () {}, child: Text("Button"),),
-      ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
-        child: Text("Click!eeee",
-          style: TextStyle(
-              fontFamily: 'IndieFlower'
+      appBar: AppBar(
+          title: Text("App bar TEXTee"),
+          centerTitle: true,
+          backgroundColor: Colors.blue[600]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Center(
+            child: Container(
+                        child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  color: Colors.blueAccent[400],
+                  padding: EdgeInsets.all(50),
+                  margin: EdgeInsets.all(50),
+                  child: Center(
+                    child: IconButton(
+                      onPressed: () {
+                        print("Clicked!!");
+                      },
+                      icon: Icon(Icons.timer, color: Colors.amber),
+                    ),
+                  ),
+                ),
+              ),
             ),
           ),
-        backgroundColor: Colors.red[600]
+          Center(child: Text('Between')),
+          Center(
+            child: FlatButton(
+              onPressed: () {},
+              child: Text("Button"),
+            ),
+          ),
+        ],
       ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Text(
+            "Click!eeee",
+            style: TextStyle(fontFamily: 'IndieFlower'),
+          ),
+          backgroundColor: Colors.red[600]),
     );
   }
 }
